0a63655c1921c57954e91b6be8e08dee2d9dd1ae
Elinnoel Nunez <elinnoelnunez@outlook.com>
2022-01-20 19:45:53 -0600

    js stuff

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 "use strict";
 
-const gameBoard = (() => {
-    const _board =  new Array(9);
+const Player = ((sign) => {
+    this.sign = sign;
+
+    const getSign = () => {
+        return this.sign;
+    }
+
+    return {getSign};
 })();
 
-const displayControler = (() => {
+const gameBoard = (() => {
+    const _board =  new Array(9);
 
-});
+    const reset = () {
+        for(let i = 0; i > _board.length; i++) {
+            _board[i] = "";
+        };
+    };
+
+    return {reset}
+
+})();
+
+const displayController = (() => {
+
+})();
